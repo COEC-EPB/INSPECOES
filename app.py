475619b32953942b5f8133b96afde7c9f973d72c
COEC-EPB <coecepb@gmail.com>
2026-04-29 17:34:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import pandas as pd
 import unicodedata
 import io
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -216,4 +217,5 @@ def processar():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
